5b5d119d6e9e2aca9f3fad695c8183b68e334da5
julesy <dev.jules.blount@gmail.com>
2023-04-05 13:23:36 -0400

    implemented base pipeline for HF transformers

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import numpy as np
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis")
 
 title = "Toxic Tweets"
 st.title(title)
 
 options = np.array(["BERT", "GPT"])
-choice = st.selectbox("Select Model", options)
+choice = st.selectbox("Select Model:", options)
 
-response = st.text_input("Enter Text")
+response = st.text_input("Enter Text to Analyse:", value="I am excited to begin working on this CS482 Project!")
 
 if st.button("Process Text"):
-    st.write("TODO Button Press. {}".format(response))
+    pred = classifier(response)
+    st.write(pred)
