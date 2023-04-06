5bf9b425228f55276fdbe63c65e3a99507987848
julesy <dev.jules.blount@gmail.com>
2023-04-06 14:44:59 -0400

    impemented a way to get probabilty amount from tensor

--- app.py
+++ app.py
@@ -16,9 +16,10 @@ title = "Toxic Tweets"
 st.title(title)
 
 options = np.array( list(models.keys()) )
-choice = str(st.selectbox("Select Model:", options))
-
+choice = str(st.selectbox("Select Base Model:", options))
 pre_model = models[choice]
+
+st.write("Model Used: ", pre_model)
 
 tokenizer = AutoTokenizer.from_pretrained(pre_model)
 model = TFAutoModelForSequenceClassification.from_pretrained(pre_model)
@@ -28,15 +29,19 @@ classifier = pipeline("text-classification", model=pre_model)
 response = st.text_input("Enter Text to Analyse:", "I am excited to begin working on this CS482 Project!")
 
 if st.button("Submit"):
-    st.header(":blue[Results]")
+    st.write(":blue[=== Results ===]")
 
     tokens = tokenizer(response, return_tensors='tf')
     outputs = model(tokens)
 
     predictions = tf.nn.softmax(outputs.logits, axis=-1)
+
+    predicted_amount = int(tf.math.reduce_max(outputs.logits, axis=-1)[0])
     predicted_class_id = int(tf.math.argmax(outputs.logits, axis=-1)[0])
 
     pred = classifier(response)
-    
     st.write(pred)
-    st.write("This sentence seems: ", model.config.id2label[predicted_class_id])
+
+    st.write(predictions)
+
+    st.write("This sentence can be interpreted as: ", predicted_amount, model.config.id2label[predicted_class_id])
