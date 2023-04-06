40b8342b9e69b4d3d3798378d417abab27023797
julesy <dev.jules.blount@gmail.com>
2023-04-06 17:01:17 -0400

    final commit for milestone-2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 
 #https://huggingface.co/course/chapter2/2?fw=pt
-from transformers import pipeline, AutoTokenizer, TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
 import tensorflow as tf
 
 models = {
@@ -24,8 +24,6 @@ st.write("Model Used: ", pre_model)
 tokenizer = AutoTokenizer.from_pretrained(pre_model)
 model = TFAutoModelForSequenceClassification.from_pretrained(pre_model)
 
-classifier = pipeline("text-classification", model=pre_model)
-
 response = st.text_input("Enter Text to Analyse:", "I am excited to begin working on this CS482 Project!")
 
 if st.button("Submit"):
@@ -36,12 +34,7 @@ if st.button("Submit"):
 
     predictions = tf.nn.softmax(outputs.logits, axis=-1)
 
-    predicted_amount = int(tf.math.reduce_max(outputs.logits, axis=-1)[0])
-    predicted_class_id = int(tf.math.argmax(outputs.logits, axis=-1)[0])
+    predicted_amount = float(tf.math.reduce_max(predictions, axis=-1)[0])
+    predicted_class_id = int(tf.math.argmax(predictions, axis=-1)[0])
 
-    pred = classifier(response)
-    st.write(pred)
-
-    st.write(predictions)
-
-    st.write("This sentence can be interpreted as: ", predicted_amount, model.config.id2label[predicted_class_id])
+    st.markdown("This sentence can be interpreted as: _:green[{:.2%}] {}_".format(predicted_amount, model.config.id2label[predicted_class_id]))
