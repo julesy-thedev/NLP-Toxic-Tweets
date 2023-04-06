aaeb7ab3fc78cb39b4fb16b559e5937bc1301e73
julesy <dev.jules.blount@gmail.com>
2023-04-05 22:35:30 -0400

    Implemented pipeline from tokenizer to output

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import numpy as np
-from transformers import pipeline
 
-classifier = pipeline("sentiment-analysis")
+#https://huggingface.co/course/chapter2/2?fw=pt
+import torch
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+
+#classifier = pipeline("sentiment-analysis")
 
 title = "Toxic Tweets"
 st.title(title)
@@ -12,7 +15,18 @@ choice = st.selectbox("Select Model:", options)
 
 response = st.text_input("Enter Text to Analyse:", "I am excited to begin working on this CS482 Project!")
 
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+
 if st.button("Submit"):
-    pred = classifier(response)
     st.header(":blue[Results]")
-    st.write(pred)
+
+    #pred = classifier(response)
+    #st.write(pred)
+
+    tokens = tokenizer(response, padding=True, truncation=True, return_tensors="pt")
+    outputs = model(**tokens)
+    predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+
+    st.write(predictions)
