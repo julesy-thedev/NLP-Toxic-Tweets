f68a62f362bb729a5208fe8061c249a6c711edaa
julesy <dev.jules.blount@gmail.com>
2023-04-06 07:39:57 -0400

    implemented dictionary for multi-model selection

--- app.py
+++ app.py
@@ -5,30 +5,34 @@ import numpy as np
 import torch
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
-#classifier = pipeline("sentiment-analysis")
+models = {
+    "BERT": "bert-base-uncased",
+    "DistilBERT": "distilbert-base-uncased-finetuned-sst-2-english"
+}
+
+classifier = pipeline("text-classification")
 
 title = "Toxic Tweets"
 st.title(title)
 
-options = np.array(["BERT", "GPT"])
-choice = st.selectbox("Select Model:", options)
+dict_keys = models.keys()
+options = np.array(dict_keys)
+choice = str(st.selectbox("Select Model:", options))
+
+tokenizer = AutoTokenizer.from_pretrained(models[choice])
+model = AutoModelForSequenceClassification.from_pretrained(models[choice])
 
 response = st.text_input("Enter Text to Analyse:", "I am excited to begin working on this CS482 Project!")
-
-#checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
-checkpoint = "bert-base-uncased"
-
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
 
 if st.button("Submit"):
     st.header(":blue[Results]")
 
-    #pred = classifier(response)
-    #st.write(pred)
+    pred = classifier(response)
+    st.write(pred)
 
     tokens = tokenizer(response, padding=True, truncation=True, return_tensors="pt")
     outputs = model(**tokens)
     predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
 
     st.write(predictions)
+    st.write(model.config.id2label)
