1a992f36528e18ff5083a484a864d7b8444efdf1
julesy <dev.jules.blount@gmail.com>
2023-04-23 21:40:49 -0400

    fixed sample tweets sampling

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ title = "Toxic Tweets"
 st.title(title)
 
 options = np.array( list(models.keys()) )
-choice = str(st.selectbox("Finetuned Model:", options))
+choice = str(st.selectbox("Custom Finetuned Model:", options))
 pre_model = models[choice]
 
 st.write("Fine Tuned DistilBert model for identifying Toxic Comments.")
@@ -21,16 +21,15 @@ st.write("Fine Tuned DistilBert model for identifying Toxic Comments.")
 tokenizer = AutoTokenizer.from_pretrained(pre_model)
 model = TFAutoModelForSequenceClassification.from_pretrained(pre_model)
 
-response = st.text_input("Enter Text to Analyse:", "I am excited to begin working on this Project!")
 sample_tweets = ["I hate you!", "you're a dumbass", "thats pretty bad", "damn, i wish i didnt do that"]
 
-if st.button("Submit"):
+if st.button("Load Tweets"):
     st.write(":blue[=== Results ===]")
 
     col1, col2, col3 = st.columns(3)
 
     for i in sample_tweets:
-        tokens = tokenizer(response, return_tensors='tf')
+        tokens = tokenizer(i, return_tensors='tf')
         outputs = model(tokens)
 
         predictions = tf.nn.softmax(outputs.logits, axis=-1)
@@ -41,5 +40,3 @@ if st.button("Submit"):
         col1.write(i)
         col2.write(model.config.id2label[predicted_class_id])
         col3.write(predicted_amount)
-
-        #st.markdown("This sentence can be interpreted as: _:green[{:.2%}] {}_".format(predicted_amount, model.config.id2label[predicted_class_id]))
