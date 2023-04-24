b20b3e140b733cf7de4971eb549d5bc759d1a9e9
julesy <dev.jules.blount@gmail.com>
2023-04-23 22:19:03 -0400

    updated display method from colums to table fix 2

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ model = TFAutoModelForSequenceClassification.from_pretrained(pre_model)
 
 sample_tweets = ["Yo bitch Ja Rule is more succesful then you'll ever be whats up with you and hating you sad mofuckas...i should bitch slap ur pethedic white faces and get you to kiss my ass you guys sicken me. Ja rule is about pride in da music man. dont diss that shit on him. and nothin is wrong bein like tupac he was a brother too...fuckin white boys get things right next time.,", "Thank you for understanding. I think very highly of you and would not revert without discussion.", "Please stop. If you continue to vandalize Wikipedia, as you did to Homosexuality, you will be blocked from editing.", "I WILL BURN YOU TO HELL IF YOU REVOKE MY TALK PAGE ACCESS!!!!!!!!!!!!!"]
 
-
 data = {
   "tweets": sample_tweets,
   "label": [],
@@ -43,8 +42,8 @@ if st.button("Load Tweets"):
         predicted_amount = float(tf.math.reduce_max(predictions, axis=-1)[0])
         predicted_class_id = int(tf.math.argmax(predictions, axis=-1)[0])
 
-        data["label"][i] = model.config.id2label[predicted_class_id]
-        data["percent"][i] = predicted_amount
+        data["label"].append(model.config.id2label[predicted_class_id])
+        data["percent"].append(predicted_amount)
 
     df = pd.DataFrame(data)
 
