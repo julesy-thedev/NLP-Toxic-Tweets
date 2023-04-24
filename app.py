97845835fff769780cd7c961e9b916d41733376d
julesy <dev.jules.blount@gmail.com>
2023-04-23 21:51:12 -0400

    updated the sample tweets from test dataset

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.write("Fine Tuned DistilBert model for identifying Toxic Comments.")
 tokenizer = AutoTokenizer.from_pretrained(pre_model)
 model = TFAutoModelForSequenceClassification.from_pretrained(pre_model)
 
-sample_tweets = ["I hate you!", "you're a dumbass", "thats pretty bad", "damn, i wish i didnt do that"]
+sample_tweets = ["Yo bitch Ja Rule is more succesful then you'll ever be whats up with you and hating you sad mofuckas...i should bitch slap ur pethedic white faces and get you to kiss my ass you guys sicken me. Ja rule is about pride in da music man. dont diss that shit on him. and nothin is wrong bein like tupac he was a brother too...fuckin white boys get things right next time.,", "Thank you for understanding. I think very highly of you and would not revert without discussion.", "Please stop. If you continue to vandalize Wikipedia, as you did to Homosexuality, you will be blocked from editing.", "I WILL BURN YOU TO HELL IF YOU REVOKE MY TALK PAGE ACCESS!!!!!!!!!!!!!"]
 
 if st.button("Load Tweets"):
     st.write(":blue[=== Results ===]")
